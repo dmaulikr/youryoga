0c6b4b7fc8dfba616b20d94d9b7289cb463d4c34
john <john@Taelmon.local>
2014-08-17 23:20:12 -0400

    Initial Commit

---
+++ YourYoga/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  YourYoga
+//
+//  Created by john on 8/17/14.
+//  Copyright (c) 2014 SaintsSoft LLC. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ YourYoga/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  YourYoga
+//
+//  Created by john on 8/17/14.
+//  Copyright (c) 2014 SaintsSoft LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
